bf6d822ec5374eb4a0229db29a641ea304741b2f
Fernando Jesus Coyla Alvarez <fcoylaa@unsa.edu.pe>
2022-05-15 11:49:49 -0500

    agregado metodo para mostrar array, array establecido como variable  de instancia

--- RotacionIzquierda.java
+++ RotacionIzquierda.java
@@ -1,15 +1,19 @@
 import java.util.Scanner;
+import java.util.Arrays;
 public class RotacionIzquierda{
+	private static int [] numeros = {1,2,3,4,5};
 	public static void main(String[] args) {
 		Scanner leer = new Scanner(System.in);
-		System.out.println("Ingrese el tamaño del arreglo");
+		System.out.println("Ingrese numero de rotacion: ");
 		int n = leer.nextInt();
-
-		int array [] = new int [n];	
-		
-		System.out.println("esto es : "+n);
+				
 	}
-	public static int [] rotacionIzquierda(int [] a){
+	public static void rotacionIzquierda(int [] a){
 		
+	}
+	public static void mostrar(){
+		for (int i = 0 ; i < numeros.length ; i++){
+			System.out.print(numeros[i] + " ");
+		}
 	}
 }
