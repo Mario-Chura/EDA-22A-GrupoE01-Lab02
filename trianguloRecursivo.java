2174a4ee16ca33f6eb991a777893d5d5bcaaec5d
Mario-Chura <mchurapum@unsa.edu.pe>
2022-05-15 10:49:15 -0500

    Anadiendo lineas para ejecutar metodo

--- trianguloRecursivo.java
+++ trianguloRecursivo.java
@@ -2,7 +2,10 @@ import java.util.Scanner;
 public class trianguloRecursivo {
 
 	public static void main(String[] args) {
-
+        System.out.println ("Ejemplo de triangulo recursivo en  Java");
+        System.out.println("Ingrese la base: ");
+        Scanner scan = new Scanner (System.in);
+        trianguloRecursivo(5, "");
 	}
     public static void trianguloRecursivo (int numeroCaracteres, String cadena) {
         if ( numeroCaracteres < 1) {
