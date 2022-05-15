3af64bfcfdcd765caa58db8f8016882e531ea953
Icielo23 <viquise@unsa.edu.pe>
2022-05-14 22:42:16 -0500

    intento

--- Invertirmatriz.java
+++ Invertirmatriz.java
@@ -2,5 +2,18 @@ public class Invertirmatriz {
         
  public static void main(String[] args) {
   
+
+ }
+ public static int[] matrizInvertida(int[] matriz) {
+     int iteraciones = (int) Math.floor(matriz.length/2);
+     int longitud = matriz.length;
+
+     for (int i = 0; i < iteraciones; i++){
+         int auxiliar = matriz[i];
+         matriz[i] = matriz[longitud - i - 1];
+         matriz[longitud - i -1] = auxiliar;
+     }
+    
+     
  }
 }
