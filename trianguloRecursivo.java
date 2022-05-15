3a0a5cc650485e8cd0ec20bf906a7b713efd6e1f
Mario-Chura <mchurapum@unsa.edu.pe>
2022-05-15 10:47:48 -0500

    Creando metodo de triangulo recursico

--- trianguloRecursivo.java
+++ trianguloRecursivo.java
@@ -1,6 +1,12 @@
+import java.util.Scanner;
 public class trianguloRecursivo {
 
 	public static void main(String[] args) {
 
 	}
+    public static void trianguloRecursivo (int numeroCaracteres, String cadena) {
+        if ( numeroCaracteres < 1) {
+            System.out.print(" ");
+        }
+    }
 }
