71bc72e6651d9a726c1613d096aee37e2c3fa2cd
Mario-Chura <mchurapum@unsa.edu.pe>
2022-05-15 12:09:42 -0500

    Agregando metodos constructores

--- Node.java
+++ Node.java
@@ -1,4 +1,17 @@
 public class Node <T{
     private T data;
 	private Node<T> nextNode;
+
+    // Metodos constructores
+	
+	public Node(T data, Node<T> nextNode){
+		this.data = data;
+		this.nextNode = nextNode;
+	}
+	public Node(T data){
+		this(data, null);
+	}
+	public Node(){
+		this(null, null);
+	}
 }
