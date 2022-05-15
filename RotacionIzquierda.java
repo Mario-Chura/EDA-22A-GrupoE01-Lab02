94c1ce23497bbbcdb25cc5a62f526d9f2c6468e0
Fernando Jesus Coyla Alvarez <fcoylaa@unsa.edu.pe>
2022-05-15 12:11:01 -0500

    ejercicio mejorado, muestra de todas las rotaciones a la izquierda que se realizan con los elementos del array

--- RotacionIzquierda.java
+++ RotacionIzquierda.java
@@ -1,28 +1,26 @@
-import java.util.Scanner;
-import java.util.Arrays;
+
 public class RotacionIzquierda{
 	private static int [] numeros = {1,2,3,4,5};
 	public static void main(String[] args) {
-		Scanner leer = new Scanner(System.in);
-		System.out.println("Ingrese numero de rotacion: ");
-		int n = leer.nextInt();
-	
+
 		System.out.println("Array");
                 mostrar();
-                rotacionIzquierda(n);
-        	System.out.println("\nArray rotado a la izquierda"+n+" vez");
-                mostrar();
+	
+		for (int i = 0 ; i < 5 ; i++){
+            		rotacionIzquierda();
+            		System.out.println("\nArray rotado a la izquierda "+ (i+1)+" vez");
+            		mostrar();
+       		}
+		System.out.println("");        	
+                
 	}
-	public static void rotacionIzquierda(int n){
-	 	
- 		for(int h = 1; h <= n ; h++){
-			int aux = numeros[0];
-			for(int i=0; i<numeros.length - 1 ; i++ ){
-				numeros[i] = numeros[i+1];
-			}
-			numeros[ numeros.length - 1] = aux;
-			aux = 0;
-		}	 
+	public static void rotacionIzquierda(){
+ 		
+		int aux = numeros[0];
+		for(int i=0; i<numeros.length - 1 ; i++ ){
+			numeros[i] = numeros[i+1];
+		}
+		numeros[ numeros.length - 1] = aux;		 
 	}
 	public static void mostrar(){
 		for (int i = 0 ; i < numeros.length ; i++){
