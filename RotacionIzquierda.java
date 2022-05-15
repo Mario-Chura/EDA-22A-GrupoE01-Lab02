467ff53daf16861c6df656991f81432b9855cf59
Fernando Jesus Coyla Alvarez <fcoylaa@unsa.edu.pe>
2022-05-14 22:52:04 -0500

    agregacion de scanner para ingresar tamaño del arreglo

--- RotacionIzquierda.java
+++ RotacionIzquierda.java
@@ -1,7 +1,12 @@
+import java.util.Scanner;
 public class RotacionIzquierda{
 	public static void main(String[] args) {
+		Scanner leer = new Scanner(System.in);
+		System.out.println("Ingrese el tamaño del arreglo");
+		int n = leer.nextInt();
+
+		int array [] = new int [8];	
 		
-		
-		System.out.println("");
+		System.out.println("esto es : "+n);
 	}
 }
